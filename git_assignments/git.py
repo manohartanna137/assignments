1f5957d2e40b2eeeb8cd1a1c750688ba30c10a78
manohartanna137 <tanna.manohar@diggibyte.com>
2023-05-11 12:20:17 +0530

    made some changes for values

--- git_assignments/git.py
+++ git_assignments/git.py
@@ -1,12 +1,14 @@
 a=input()
-c=0
+c=1
 l=len(a)
 while c<(len(a)):
   print(a[c])
   c=c+1
 
-  n = int(input())
-  c = 0
-  while c < n:
-    print('*' * n)
-    c = c + 1
+n = int(input())
+c = 1
+while c < n:
+  print('*' * n)
+  c = c + 1
+
+# print("Hi")
