edbc32703bf3c957f9e99fd8740da61c5fee8177
manohartanna137 <tanna.manohar@diggibyte.com>
2023-05-17 19:34:17 +0530

    This is test file for percentage program

--- python_asignments/percentage_problem/test/test.py
+++ python_asignments/percentage_problem/test/test.py
@@ -7,6 +7,10 @@ class MyTestCase(unittest.TestCase):
         expectedoutput=20.0
         self.assertEqual( actualinput,expectedoutput)  # add assertion here
 
+    def test_somethin(self):
+        actualinput = percentage_calculation({'a': [10, 20, 50], 'b': [30, 60, 40]}, 'a')
+        expectedoutput = 26.666666666666668
+        self.assertEqual(actualinput, expectedoutput)
 
 if __name__ == '__main__':
     unittest.main()
