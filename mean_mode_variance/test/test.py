a7592c758480bbd3ba6290554eaa3cb57c370514
manohartanna137 <tanna.manohar@diggibyte.com>
2023-05-18 18:22:21 +0530

    This is a test file for mean mode standard deviation programm

---
+++ mean_mode_variance/test/test.py
@@ -0,0 +1,28 @@
+
+import mean_mode_variance.core.utils as m
+import unittest
+import numpy as np
+
+
+class test_sample(unittest.TestCase):
+
+    def test_mean(self):
+        arr = np.array([[1, 2], [3, 4]])
+        result = m.arr_mean(arr)
+        p = np.array([1.5, 3.5])
+        self.assertIsNone(np.testing.assert_almost_equal(result, p, 1))
+
+    def test_var(self):
+        arr = np.array([[1, 2], [3, 4]])
+        q = np.array([1., 1.])
+        result = m.arr_var(arr)
+        self.assertIsNone(np.testing.assert_almost_equal(result, q, 1))
+
+    def test_std(self):
+        arr = np.array([[1, 2], [3, 4]])
+        result = m.arr_std(arr)
+        self.assertEqual(result, 1.11803398875)
+
+
+if __name__ == '__main__':
+    unittest.main()
