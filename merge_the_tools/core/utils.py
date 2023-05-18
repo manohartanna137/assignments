554edbbfa23ab0362f6d78812d4641a0f7a42dd9
manohartanna137 <tanna.manohar@diggibyte.com>
2023-05-18 18:23:33 +0530

    This is a utils file for merge tools programm

--- merge_the_tools/core/utils.py
+++ merge_the_tools/core/utils.py
@@ -5,7 +5,7 @@ def merge_the_tools(string, k):
         for j in range(k):
             if string[i + j] not in sub_string:
                 sub_string = sub_string + string[i + j]
-            else:
+            else:*
                 continue
         list1.append(sub_string)
     return list1
