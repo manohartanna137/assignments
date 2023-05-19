512e8ea7a8521f5529359e813ce939bfda73f65d
manohartanna137 <tanna.manohar@diggibyte.com>
2023-05-19 16:34:22 +0530

    This is driver file for regex pattern programm

---
+++ python_asignments/regex_pattern/core/driver.py
@@ -0,0 +1,5 @@
+from regex_pattern.core.utils import *
+emails = ['user@example.com','user@domain.co.in','user.name@example','user123@website','invalid_email','user@','@domain.com']
+for x in emails:
+    if validate_email(x):
+        print(x)
